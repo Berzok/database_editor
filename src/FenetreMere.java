0657fae510b11ca2b63899ced544075743f06a82
Berzok <34751752+Berzok@users.noreply.github.com>
2018-02-13 11:29:26 +0100

    Add files via upload

--- src/FenetreMere.java
+++ src/FenetreMere.java
@@ -5,13 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.*;
-import java.sql.*;
 
-public class FenetreMere extends JFrame
+public class FenetreMere extends JFrame implements ActionListener
 	{
 	static String chPseudo;
 	static String chMotdepasse;
-	Connection chCo;
 	/**
 	 * 
 	 */
@@ -32,24 +30,18 @@ public class FenetreMere extends JFrame
 		AskingYou whoAreYou = new AskingYou("Connexion...", laFenetre);
 		}
 	
-	public static void createInterface(FenetreMere parFenetre, String parPseudo)
-		{
-		InterfaceG leContenu = new InterfaceG(parFenetre, parPseudo);
-		}
-	
-	public void createConnexion (FenetreMere parFenetre, String parPseudo, String parPassword, AskingYou parYou) throws SQLException, ClassNotFoundException
+	public void createConnexion (String parPseudo, String parPassword, AskingYou parYou) throws SQLException, ClassNotFoundException
 		{
 		chPseudo = parPseudo; chMotdepasse = parPassword;
 		parYou.dispose();
 		String url = "jdbc:oracle:thin:@madere:1521:info";
 		DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
 		Connection chCo = DriverManager.getConnection(url, chPseudo, chMotdepasse);
-		createInterface(parFenetre, chPseudo);
-		this.revalidate();
 		}
-	public void actualiser()
+	
+	public void actionPerformed(ActionEvent parEvent)
 		{
-		this.revalidate();
+		
 		}
 	}
 //Statement stmt = con.createStatement()
--- src/InterfaceG.java
+++ src/InterfaceG.java
@@ -1,55 +1,30 @@
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.*;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-public class InterfaceG extends JPanel implements ActionListener
+public class InterfaceG extends JFrame implements ActionListener
 	{
-	JTextField chSaisieRequete;
-	String chRequete;
-	JTextArea chResultatRequete;
-	JButton chBouton;
-	FenetreMere chMommy;
-	public InterfaceG(FenetreMere parFenetre, String parPseudo)
+	static String[] user;
+	public InterfaceG(String parTitre)
 		{
-		chMommy = parFenetre;
-		JPanel lePanel2 = new JPanel(); lePanel2.setLayout(new BoxLayout(lePanel2, BoxLayout.LINE_AXIS));
-		JLabel leUser = new JLabel("Utilisateur: " + parPseudo); lePanel2.add(leUser);
-		JPanel lePanel3 = new JPanel(); lePanel3.setLayout(new BoxLayout(lePanel3, BoxLayout.LINE_AXIS));
-		chSaisieRequete = new JTextField(20); lePanel3.add(chSaisieRequete);
-		chBouton = new JButton("Action !"); chBouton.addActionListener(this);
-		lePanel3.add(chBouton);
-		JPanel lePanel4 = new JPanel(); lePanel4.setLayout(new BoxLayout(lePanel4, BoxLayout.LINE_AXIS));
-		chResultatRequete = new JTextArea(); chResultatRequete.setEditable(false);
-		lePanel4.add(chResultatRequete);
-		JPanel lePanel = new JPanel(); lePanel.setLayout(new BoxLayout(lePanel, BoxLayout.PAGE_AXIS));
-		lePanel.add(lePanel2);
-		lePanel.add(lePanel3);
-		lePanel.add(lePanel4);
+		super(parTitre);
+		String[] user = new String[2];
+		this.setSize(400, 200);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
-		parFenetre.add(lePanel);
+		this.actualiser();
+		}
+	public void askingYou(String[] parUser)
+		{
+		
+		}
+	public void actualiser()
+		{
+		this.revalidate();
 		}
 	public void actionPerformed(ActionEvent parEvent)
 		{
-		if(parEvent.getSource() == chBouton)
-			{
-			chRequete = chSaisieRequete.getText();
-			Statement stmt;
-			try
-				{
-				stmt = chMommy.chCo.createStatement();
-				ResultSet resultat = stmt.executeQuery(chRequete);
-				chResultatRequete.setText(chRequete);
-				} 
-			catch (SQLException e1)
-				{
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-				}
-		chMommy.actualiser();
-		}
+		
 		}
 	}
